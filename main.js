07bb42700bc0cd4374e44030bf1901e22255f31f
Błażej Supiński <berekeks@gmail.com>
2019-06-25 21:46:50 +0200

    add flex box; gradient

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const tesc = document.querySelector('.section__six--js');
 tesc.innerHTML = "Java Script != Java";
+
+
+function nazwaFunkcji (imie, wiek){
+    console.logs(`Witaj ${imie} lat ${wiek}`)
+
+}
+
+nazwaFunkcji('blazj',37)
