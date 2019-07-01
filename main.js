ba91aaf33e27f4beee0751c51e030a721309402a
Błażej Supiński <berekeks@gmail.com>
2019-07-01 22:00:26 +0200

    media queris, hamburger menu

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ tesc.innerHTML = "Java Script != Java";
 
 
 function nazwaFunkcji (imie, wiek){
-    console.logs(`Witaj ${imie} lat ${wiek}`)
+    console.log(`Witaj ${imie} lat ${wiek}`)
 
 }
 
-nazwaFunkcji('blazj',37)
+nazwaFunkcji('blazj',37);
+
+const pstryczek = document.querySelector('.button__hamburger');
+pstryczek.addEventListener('click', (e) => {
+    const nawigacja = document.querySelector('.ulList');
+    nawigacja.classList.toggle('visible')
+})
